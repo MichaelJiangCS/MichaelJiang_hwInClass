8a1af2ebe817f0a601726fd691982a0c4a3ade76
Michael <michael.jiang799@gmail.com>
2023-10-24 15:21:38 -0400

    1

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ const buttonA = document.getElementById('buttonA');
 const hideA = document.getElementById("hideA");
 const divA = document.getElementById("divA");
 
-//buttonA.addEventListener("click", (e)=> {
-//    document.getElementById('imageA').style.display="block";
-//});
-
 
 hideA.addEventListener("click", (e)=> {
     divA.style.display = "none";
+});
+
+buttonA.addEventListener("click", (e)=> {
+    divA.style.display = "initial";
 });
